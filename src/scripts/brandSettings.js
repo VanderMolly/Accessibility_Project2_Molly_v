6124a598454727d45c89548ad66bf14d8e326c06
Molly Vanderbogart <vanderbm1@newpaltz.edu>
2026-03-04 23:07:44 +0000

    changing assets

--- src/scripts/brandSettings.js
+++ src/scripts/brandSettings.js
@@ -1,32 +1,32 @@
 export const brandSettings = {
     logo: {
-        src: "/Assets/logo.jpg", // Add logo image URL here (e.g., "/assets/logo.png")
+        src: "/assets/logo.jpg", // Add logo image URL here (e.g., "/assets/logo.png")
         alt: "New York Times Cooking",
         variations: [ // you might have wordmarks / different color variants than what is listed here, that's fine just adjust it
             { 
                 name: "Primary - Light Background", 
-                src: "/Assets/lightlogo.png", // Add image path
+                src: "/assets/lightlogo.png", // Add image path
                 bg: "#ffffff", 
                 darkText: true,
                 description: "Use this version on white or light backgrounds."
             },
             { 
                 name: "Primary - Dark Background", 
-                src: "/Assets/white.png", // Add image path
+                src: "/assets/white.png", // Add image path
                 bg: "#333333", 
                 darkText: false,
                 description: "Use this version on dark or brand-colored backgrounds."
             },
             { 
                 name: "Monochrome - Black", 
-                src: "/Assets/black.png", // Add image path
+                src: "/assets/black.png", // Add image path
                 bg: "#ffffff", 
                 darkText: true,
                 description: "For black and white print or single-color applications."
             },
             { 
                 name: "Monochrome - White", 
-                src: "/Assets/white.png", // Add image path
+                src: "/assets/white.png", // Add image path
                 bg: "#000000", 
                 darkText: false,
                 description: "For dark backgrounds where color is not permitted."
